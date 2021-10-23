944f011fc164e6ae767cbb9b0c6204079262dbdb
Stern <stern@mail.com>
2021-10-22 18:13:56 -0700

    Files Added

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/denori/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/denori/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/denori/catkin_ws/devel;/opt/ros/noetic' != '' else []
